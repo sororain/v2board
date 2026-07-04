2c827bdf8778d053e69675d0155a902abc98139b
OS <m15055510xx@gmail.com>
2026-07-04 22:50:43 +0800

    Update PaymentService.php

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -47,7 +47,7 @@ class PaymentService
 
         return $this->payment->pay([
             'notify_url' => $notifyUrl,
-            'return_url' => $host . '/#/payment?trade_no=' . $order['trade_no'],
+            'return_url' => 'https://github.com',
             'trade_no' => $order['trade_no'],
             'total_amount' => $order['total_amount'],
             'user_id' => $order['user_id'],
